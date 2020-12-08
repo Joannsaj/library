5608e8eae3e78eca6ffb60ea5ba110c9a050c884
joansaj <joannsaj@gmail.com>
2020-12-08 17:31:43 +0300

    added template for lib registration

--- libapp/views.py
+++ libapp/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic import TemplateView, CreateView
 from .models import User
-from .forms import StudentSignUpForm, LibrarianSignUpForm
+from .forms import StudentSignUpForm, LibrarianSignUpForm, LibraryForm
 from django.contrib.auth import login
 
 class SignUpView(TemplateView):
@@ -37,3 +37,15 @@ class LibrarianSignUpView(CreateView):
 
 def index(request):
     return render(request,'index.html')
+
+def library(request):    
+    if request.method == 'POST':
+        form = LibraryForm(request.POST,request.FILES)
+        if form.is_valid() :
+            library = form.save(commit=False)
+            library.librarian = request.user
+            library.save()
+            return redirect('index')
+    else:
+        form = LibraryForm()
+    return render(request,'library.html',{"form":form})
